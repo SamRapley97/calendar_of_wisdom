81e6b8a6465e06a15277f031dfb77482c2094e59
Sam <samrapley@protonmail.com>
2024-05-13 16:56:21 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    serverRuntimeConfig: {
+        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+        region: process.env.NEXT_PUBLIC_AWS_REGION,
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+      staticFolder: '/static',
+    },
+  }
+
 export default nextConfig;
