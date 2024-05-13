067c4fadf13d12c6835d70e918b066310a249df7
Sam <samrapley@protonmail.com>
2024-05-13 09:10:41 +0100

    separated out into components, rendering header text, next job buttons

--- src/app/components/TextContent.tsx
+++ src/app/components/TextContent.tsx
@@ -2,18 +2,10 @@
 
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Dropdown from '../components/DropdownButton';
-import AWS from 'aws-sdk';
-import ReactHtmlParser from 'html-react-parser';
+import ButtonGroup from './ButtonGroup';
+import ChangeTextContent from '../hooks/ChangeTextContent';
 import dayjs from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
-import 'dotenv/config'
-
-
-
-
-
 
 
 dayjs.extend(advancedFormat)
@@ -21,63 +13,32 @@ dayjs.extend(advancedFormat)
 
 
 const TextContent: React.FC = () => {
-  const [textContent, setTextContent] = useState<string>('');
+  const [textContent, setTextContent] = useState<any>('');
+  const [dayText, setDayText] = useState<any>('');
   const today = `${dayjs().format('MMMM').toLowerCase()}_${dayjs().format('Do').toLowerCase()}`
 
 
-  const ChangeDate = (selectedDate: string) => {
+  
+  React.useEffect(() => {
+    ChangeTextContent(today, setTextContent);
+    setDayText(`${dayjs().format('MMMM')} ${dayjs().format('Do')}`)
+  }, [today]);
 
   
-    
-    // useEffect(() => {
-      // Configure AWS SDK
-      AWS.config.update({
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-        region: process.env.NEXT_PUBLIC_AWS_REGION,
-      });
-
-      console.log(process.env.NEXT_PUBLIC_AWS_REGION,)
-
-      // Create an S3 object
-      const s3 = new AWS.S3();
-  
-      // Specify the parameters for retrieving the HTML file
-      const params = {
-        Bucket: 'calendar-of-wisdom',
-        Key: selectedDate + '.html'
-      };
-  
-      // Read the HTML file from S3
-      s3.getObject(params, (err, data) => {
-        if (err) {
-          console.error('Error reading HTML file:', err);
-          setTextContent('')
-          return;
-        }
-  
-        // Update the date content with the HTML file content
-        setTextContent(ReactHtmlParser(data.Body?.toString()));
-      });
-    // }, []); // Empty dependency array to run the effect only once on component mount
+  const handleDateChange = (selectedDate: string) => {
+    // Call ChangeTextContent function with the selected date and setTextContent
+    ChangeTextContent(selectedDate, setTextContent);
+    let formattedSelectedDate = selectedDate.charAt(0).toUpperCase()+ selectedDate.slice(1)
+    formattedSelectedDate = formattedSelectedDate.replace(/_/g, ' ')
+    setDayText(formattedSelectedDate)
   };
 
-
-  // React.useEffect(() => {
-  //   ChangeDate(today); // Example date
-  // }, [today]);
 
 
   return (
     <main>
-      <section className="buttonGroup">
-        <Link href="/about" passHref>
-          <button className="navButton">
-            About
-          </button>
-        </Link>
-        <Dropdown handleDateChange={ChangeDate} />
-      </section>
+      <ButtonGroup handleDateChange={handleDateChange}></ButtonGroup>
+      <h1>{dayText}</h1>
       {textContent}
     </main>
   );
